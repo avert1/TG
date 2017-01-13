9dbb282b50792654e73e55dce8711d3c2431e2b5
Avert <avert123@gmail.com>
2017-01-13 04:39:28 -0500

    Initial commit of generated libgdx code

---
+++ android/src/com/avert1/tg/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.avert1.tg;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.avert1.tg.MyGdxGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MyGdxGame(), config);
+	}
+}
---
+++ desktop/src/com/avert1/tg/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.avert1.tg.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.avert1.tg.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
